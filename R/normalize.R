60f84e0bbc9a3aeae6a950636a2d742838a19813
Tobias Schmidt <royalts@gmail.com>
2017-11-14 16:57:44 -0800

    initial commit

---
+++ R/normalize.R
@@ -0,0 +1,47 @@
+#' Normalize factor loadings
+#'
+#' Normalize factor loadings using different methods
+#'
+#' @param factor_df a data.frame in long format with at least three columns: "user_id" or "item_id", "factor" and "loading"
+#' @param method the normalization method. Possible options:
+#' - raw
+#' - demean
+#' - exp_normalized
+#' - percent
+#'
+#' @export
+#'
+normalize_factor_loadings <- function(factor_df, method){
+  valid_methods <- c('raw', 'demean','exp', 'percent')
+  if(!(method %in% valid_methods)) {
+    stop('normalization_method must be raw, demean, exp_normalized or percent')
+  }
+
+  grouping_regex <- '(item|user)\\_id'
+
+  if(method == 'raw')  {
+    factor_df -> factor_df_out
+  } else {
+    factor_df %>%
+      group_by_at(vars(matches(grouping_regex))) -> factor_grouped
+
+    if(method == 'demean'){
+      factor_grouped %>%
+        mutate(loading = loading - mean(loading)) %>%
+        ungroup -> factor_df_out
+    } else if(method == 'exp'){
+      factor_grouped %>%
+        mutate(loading =  exp(loading - max(loading)) / sum(exp(loading - max(loading)))) %>%
+        ungroup -> factor_df_out
+    } else if(method == 'percent'){
+      factor_grouped %>%
+        mutate(loading =  loading / sum(loading)) %>%
+        ungroup -> factor_df_out
+    }
+  }
+
+  # attach the normalization that was applied to the data.frame, to be used by e.g. plotting methods
+  attr(factor_df_out, 'normalization') <- method
+
+  factor_df_out
+}
---
+++ R/top_n.R
@@ -0,0 +1,20 @@
+#' Top n by factor
+#'
+#' Returns the top n items/users by loading
+#'
+#' @param factor_df a data.frame in long format with at least three columns: "item_id", "factor" and "loading"
+#' @param covariate_df a data.frame with covariates that will join against factor_df on item_id
+#' @param include_columns character vector of variable names to include in the table
+#' @param n how many items to return per factor
+
+#' @export
+#'
+top_n_table <- function(factor_df, covariate_df, include_columns = NULL, n = 10) {
+  factor_df %>%
+    left_join(covariate_df, by='item_id') %>%
+    arrange(factor_id, desc(loading)) %>%
+    group_by(factor_id) %>%
+    top_n(10, loading) %>%
+    ungroup %>%
+    select(one_of(c('factor_id', 'loading', include_columns)))
+}
