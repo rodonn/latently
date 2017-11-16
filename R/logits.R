10a4bb7ea8999c5322348a0e9f06a14877bf2828
Tobias Schmidt <royalts@gmail.com>
2017-11-16 11:19:06 -0800

    port improvements to t-tests over to logits

--- R/logits.R
+++ R/logits.R
@@ -8,6 +8,40 @@
 #' @param ntiles how many ntiles to split the factor loadings into
 #' @export
 logit_heatmap <- function(factor_df, covariate_df, covariates, ntiles = 10) {
+  # call run_logits() with the same arguments the current function was called with
+  Call <- sys.call()
+  Call[[1]] <- run_logits
+  logit_results <- eval(Call, parent.frame())
+
+  n_factors <- length(unique(logit_results$factor_id))
+
+  # plot the result of the above logits as a heatmap
+  logit_results %>%
+    filter(term != '(Intercept)') %>%
+    ggplot2::ggplot(aes(factor_id, term, fill = statistic)) +
+    ggplot2::geom_tile() +
+    ggplot2::xlab('latent factor #') + ylab('') +
+    ggplot2::labs(caption = paste0("difference in mean between top and bottom ",
+                                   ntile_name(ntiles),
+                                   ",\n test statistic as text, shaded by z-statistic")) +
+    ggplot2::scale_fill_distiller(palette = "RdBu",
+                                  limits = c(-1,1) * max(abs(logit_results$statistic)),
+                                  name = 'z-statistic',
+                                  direction = -1) +
+    ggplot2::geom_text(aes(label = round(estimate, 2)), color = 'white', size = 2) +
+    ggplot2::coord_cartesian(xlim = c(1, n_factors))
+}
+
+#' Logits for differences in covariates
+#'
+#' Performs the logit estimations for logit_heatmap
+#'
+#' @param factor_df a data.frame in long format with at least three columns: "item_id", "factor" and "loading"
+#' @param covariate_df a data.frame with covariates that will join against factor_df on item_id
+#' @param covariates character vector of covariate names to perform the t-tests within
+#' @param ntiles how many ntiles to split the factor loadings into
+#'
+run_logits <- function(factor_df, covariate_df, covariates, ntiles) {
   # split the items into ntiles, then throw away everything but the top and bottom ntile
   factor_df %>%
     get_top_bottom_ntile_by_factor(ntiles) -> items_top_bottom
@@ -24,19 +58,6 @@ logit_heatmap <- function(factor_df, covariate_df, covariates, ntiles = 10) {
                        family = binomial(link = logit)))) %>%
     ungroup -> logit_results
 
-  n_factors <- length(unique(factor_df$factor_id))
-
-  # plot the result of the above logits as a heatmap
-  logit_results %>%
-    filter(term != '(Intercept)') %>%
-    ggplot(aes(factor_id, term, fill = abs(statistic))) +
-    geom_tile() +
-    xlab('latent factor #') + ylab('') +
-    labs(title = paste0("difference in mean between top and bottom ",
-                        ntile_name(ntiles),
-                        ",\n shaded by z-statistic")) +
-    theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_viridis_c(name = 'abs(z-statistic)') +
-    geom_text(aes(label = round(estimate, 2)), color = 'white', size = 2) +
-    coord_cartesian(xlim = c(1, n_factors))
+  logit_results
 }
+
