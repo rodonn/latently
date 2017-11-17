d5217f626c5289b851be0a95887ba15ce0ae8014
Tobias Schmidt <royalts@gmail.com>
2017-11-17 09:16:09 -0800

    allow normalization by both total user and total item factor loadings

--- R/pfmul_normalizations.R
+++ R/pfmul_normalizations.R
@@ -1,23 +1,24 @@
 #' Normalize PFMUL factor loadings by total user loadings
 #'
 #' @param factor_df a data.frame in long format with at least three columns: "user_id" or "item_id", "factor_id" and "loading"
-#' @param user_factor_df the data.frame of _user_ factor loadings in long format. Must have columns user_id, factor_id and "loading"
+#' @param normalize_by_factor_df the data.frame of factor loadings to normalize by in long format. Must have columns "user_id" or "item_id", "factor_id" and "loading"
 #'
 #' @export
 #'
-user_normalize_pfmul_loadings <- function(factor_df, user_factor_df) {
+user_normalize_pfmul_loadings <- function(factor_df, normalize_by_factor_df) {
   # calculate the total user loadings on each factor
-  user_factor_df %>%
+  normalize_by_factor_df %>%
     group_by(factor_id) %>%
     summarize(total_loading = sum(loading)) -> total_factor_loadings
 
-  # When fed a user factor loading data.frame, divide by the total loading
-  # within factor, if being fed an item factor loading data.frame multiply
-  operator <- ifelse('user_id' %in% colnames(factor_df), '/', '*')
+  # if factor_df and normalize_by_factor_df are identical (e.g. if item loadings
+  # are to be normalized by total item loadings) normalize by _dividing_ by the
+  # total factor loadings. If they differ, _multiply_
+  operator <- ifelse(identical(factor_df, normalize_by_factor_df), '/', '*')
 
   # divide each user/item loading by the total user loading within factor
   factor_df %>%
-    left_join(total_factor_loadings, by='factor_id') %>%
+    left_join(total_factor_loadings, by = 'factor_id') %>%
     mutate(loading = do.call(operator, list(loading, total_loading))) %>%
     select(-total_loading) -> factor_df_out
 
